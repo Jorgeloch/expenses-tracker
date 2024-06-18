fd1719e24d25e113320306320e041a559191090b
Jorge Loch <jorgembloch2001@gmail.com>
2024-06-18 14:37:33 -0300

    fix: fixing debtor dtos

--- internal/dto/debtor/createDebtorDto.go
+++ internal/dto/debtor/createDebtorDto.go
@@ -1,8 +1,5 @@
 package debtorDTO
 
-import "github.com/google/uuid"
-
 type CreateDebtorDTO struct {
-	Name    string    `json:"name" validate:"required"`
-	OwnerID uuid.UUID `json:"owner_id" validate:"required,uuid"`
+	Name string `json:"name" validate:"required"`
 }
--- internal/services/debtor/debtorService.go
+++ internal/services/debtor/debtorService.go
@@ -1,6 +1,8 @@
 package debtorService
 
 import (
+	"errors"
+
 	"github.com/google/uuid"
 	debtorDTO "github.com/jorgeloch/expenses-tracker/internal/dto/debtor"
 	debtorModel "github.com/jorgeloch/expenses-tracker/internal/models/debtor"
@@ -27,35 +29,44 @@ func (s *Service) GetByID(ownerID string, id string) (debtorModel.Debtor, error)
 
 func (s *Service) Create(ownerID string, dto debtorDTO.CreateDebtorDTO) (uuid.UUID, error) {
 	location := uuid.New()
+
 	ownerID_uuid, err := uuid.Parse(ownerID)
+	if err != nil {
+		return uuid.Nil, err
+	}
 
 	err = s.Repository.DebtorRepository.Create(debtorModel.Debtor{
 		ID:      location,
 		Name:    dto.Name,
 		OwnerID: ownerID_uuid,
 	})
-
 	if err != nil {
 		return uuid.Nil, err
 	}
-
 	return location, nil
 }
 
 func (s *Service) Update(ownerID string, id string, dto debtorDTO.UpdateDebtorDTO) (debtorModel.Debtor, error) {
 	debtor, err := s.GetByID(ownerID, id)
-
+	if debtor.ID == uuid.Nil {
+		return debtorModel.Debtor{}, errors.New("debtor not found or debtor is not yours")
+	}
 	if err != nil {
 		return debtorModel.Debtor{}, err
 	}
-
 	if dto.Name != "" {
 		debtor.Name = dto.Name
 	}
-
 	return debtor, s.Repository.DebtorRepository.Update(debtor)
 }
 
 func (s *Service) Delete(ownerID string, id string) error {
+	debtor, err := s.GetByID(ownerID, id)
+	if debtor.ID == uuid.Nil {
+		return errors.New("debtor not found or debtor is not yours")
+	}
+	if err != nil {
+		return err
+	}
 	return s.Repository.DebtorRepository.Delete(ownerID, id)
 }
